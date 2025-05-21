5bc09f738235c33640de11a9ab7436f6ed65e9f3
metilnodes <metilnodes@gmail.com>
2025-05-21 08:45:33 +0000

    описани2е изменений

--- app/head.tsx
+++ app/head.tsx
@@ -8,11 +8,13 @@ export default function Head() {
   "imageUrl": "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/testiconUrl.png",
   "button": {
     "title": "Piggy World",
-    "action": {
-      "type": "launch_frame",
-      "url": "https://v0-farcaster-app-test-publish.vercel.app",
-      "name": "Piggy World"
-    }
+    "action": "post",
+    "target": "https://v0-farcaster-app-test-publish.vercel.app/api/frame-action"
+  },
+  "action": {
+    "type": "launch_frame",
+    "url": "https://v0-farcaster-app-test-publish.vercel.app",
+    "name": "Piggy World"
   }
 }`}
       />
